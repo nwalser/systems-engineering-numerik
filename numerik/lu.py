3bdff1080eb57caa07ed5c87edd612d4f394d3cc
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2024-06-15 12:00:08 +0200

    added some solutions to cheat sheet

--- numerik/lu.py
+++ numerik/lu.py
@@ -2,19 +2,9 @@ import numpy as np
 
 
 def forward_substitution(L, b):
-    # Get number of rows
     n = L.shape[0]
-
-    # Allocating space for the solution vector
     y = np.zeros_like(b, dtype=np.double)
-
-    # Here we perform the forward-substitution.
-    # Initializing  with the first row.
     y[0] = b[0] / L[0, 0]
-
-    # Looping over rows in reverse (from the bottom  up),
-    # starting with the second to last row, because  the
-    # last row solve was completed in the last step.
     for i in range(1, n):
         y[i] = (b[i] - np.dot(L[i, :i], y[:i])) / L[i, i]
 
@@ -22,19 +12,9 @@ def forward_substitution(L, b):
 
 
 def back_substitution(U, y):
-    # Number of rows
     n = U.shape[1]
-
-    # Allocating space for the solution vector
     x = np.zeros_like(y, dtype=np.double)
-
-    # Here we perform the back-substitution.
-    # Initializing with the last row.
     x[-1] = y[-1] / U[-1, -1]
-
-    # Looping over rows in reverse (from the bottom up),
-    # starting with the second to last row, because the
-    # last row solve was completed in the last step.
     for i in range(n - 2, -1, -1):
         x[i] = (y[i] - np.dot(U[i, i:], x[i:])) / U[i, i]
 
